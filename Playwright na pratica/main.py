8e6f92df7d5a9852b53a6f483584268e5e3a3740
Lucas Pereira <88951189+Luc45-Pereira@users.noreply.github.com>
2021-12-25 21:47:14 -0300

    Update main.py

--- Playwright na pratica/main.py
+++ Playwright na pratica/main.py
@@ -1,24 +1,33 @@
 import asyncio
 from playwright.async_api import async_playwright
+import re
 
 async def main():
     async with async_playwright() as p:
         browser = await p.chromium.launch(headless=False, slow_mo=1000)
         page = await browser.new_page()
-        await page.goto("https://projetogerar.000webhostapp.com/")
-        print(await page.title())
+        await page.goto("https://www.mercadolivre.com/jms/mlb/lgz/msl/login/H4sIAAAAAAAEAzXOwQrDIBAE0H-ZsyR3j_2RsDGrkWqVdVNbQv692NLjwPBmTqQS4mPRd2VY8Kum6KLCoCZSXyQvcYNFTjBoUfkf11EhoczK0mDPAQXebuyLDErlYBjQofviU-mwvykYhAKLXbU2O8-99ymzONpKik_hyZU8rTLDQDjEpiw8Hny9y8BT00WF3B3WU2p8fQC9dzgpxAAAAA/user")
 
-        await page.fill("id=resp", "Olá, serve sim!")
+        loop = True
 
-        await page.click("//html/body/main/form/input[2]")
+        while (loop == True):
+            try:
+                await page.click('//*[@id="nav-header-menu"]/div/label/a')
 
-        codigo = await page.query_selector("//html/body/main/form/div/h6")
-        print(await codigo.inner_text())
+                await page.goto("https://www.mercadolivre.com.br/perguntas/vendedor")
 
-        pergunta = await page.query_selector("//html/body/main/form/div/h4")
+                loop = False
 
-        print(await pergunta.inner_text())
+            except:
+                loop = True
 
+        try:
+            number_question = await page.query_selector('//*[@id="page"]/div[4]/div[3]/div')
+            number_questions = re.sub('[^0-9]', '', await number_question.inner_text())
+        except:
+            print("erro")
+
+        print(number_questions)
         await browser.close()
 
 asyncio.run(main())
